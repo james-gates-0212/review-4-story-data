88a1d9d2c3f527a038b35bfbf314da06307ff962
james-gates-0212 <james.gates.0212@gmail.com>
2024-03-21 16:26:01 -0700

    config: update next.js for tunnel-ssh, sequelize⚡️
    Co-Authored-By: RR <157709103+OskarMast@users.noreply.github.com>
    Co-Authored-By: mirror0000 <134664641+mirror0000@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
     const commitHash = ChildProcess.execSync('git log --pretty=format:"%h" -n1').toString().trim();
     return process.env.GIT_HASH || commitHash;
   },
+  webpack: (config, context) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: 'node-loader',
+    });
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['sequelize'],
+  },
 };
 
 module.exports = nextConfig;
