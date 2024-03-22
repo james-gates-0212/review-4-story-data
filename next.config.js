a94cdd615e104c94c1f4b8fe1bddd2926e15fe4e
james-gates-0212 <james.gates.0212@gmail.com>
2024-03-22 06:09:30 -0700

    config: update next.config.js and .env.example📦
    Co-Authored-By: RR <157709103+OskarMast@users.noreply.github.com>
    Co-Authored-By: mirror0000 <134664641+mirror0000@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const nextConfig = {
     const commitHash = ChildProcess.execSync('git log --pretty=format:"%h" -n1').toString().trim();
     return process.env.GIT_HASH || commitHash;
   },
-  webpack: (config, context) => {
-    config.module.rules.push({
-      test: /\.node$/,
-      loader: 'node-loader',
-    });
-    return config;
-  },
+  // webpack: (config, context) => {
+  //   config.module.rules.push({
+  //     test: /\.node$/,
+  //     loader: 'node-loader',
+  //   });
+  //   return config;
+  // },
   experimental: {
     serverComponentsExternalPackages: ['sequelize'],
   },
