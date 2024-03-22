a8b0a136079d3563077d3dceb0f97ca023518338
james-gates-0212 <james.gates.0212@gmail.com>
2024-03-22 05:03:08 -0700

    fix: update tags page🐞
    Co-Authored-By: RR <157709103+OskarMast@users.noreply.github.com>
    Co-Authored-By: mirror0000 <134664641+mirror0000@users.noreply.github.com>

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -4,10 +4,29 @@ import Input from '@/components/Input';
 import Option from '@/components/Option';
 import TagInput from '@/components/TagInput';
 import TextArea from '@/components/TextArea';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-export default function Form(props) {
-  const [data, setData] = useState(props.data);
+const LoadSpin = () => (
+  <div className="fixed top-0 right-0 bottom-0 left-0 z-50 bg-black/50 flex flex-row gap-2 items-center justify-center">
+    <svg
+      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+    Processing...
+  </div>
+);
+
+export default function Form() {
+  const [data, setData] = useState(null);
 
   const [loading, setLoading] = useState(false);
 
@@ -18,14 +37,13 @@ export default function Form(props) {
 
   const onSaveTagsAndLoadNextStory = () => {
     setLoading(true);
-    console.log(keywords?.current?.getTags());
     fetch('/api/put-tags', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        storyid: data.storyid,
+        storyid: data?.storyid,
         keywords: keywords?.current?.getTags() || [],
         locations: locations?.current?.getTags() || [],
         settings: settings?.current?.getTags() || [],
@@ -41,8 +59,22 @@ export default function Form(props) {
       .finally(() => setLoading(false));
   };
 
-  if (!props?.data) {
-    return null;
+  useEffect(() => {
+    setLoading(true);
+    fetch('/api/first-story', {
+      method: 'GET',
+    })
+      .then(async (response) => {
+        setData(await response.json());
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (!data) {
+    return <LoadSpin />;
   }
 
   return (
@@ -97,24 +129,7 @@ export default function Form(props) {
           </div>
         </div>
       </div>
-      {loading && (
-        <div className="fixed top-0 right-0 bottom-0 left-0 z-50 bg-black/50 flex flex-row gap-2 items-center justify-center">
-          <svg
-            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-          Processing...
-        </div>
-      )}
+      {loading && <LoadSpin />}
     </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
+'use client';
+
 import Form from '@/app/form';
-import StoryDataService from '@/database/services/StoryDataService';
 
-export default async function Home() {
-  const service = new StoryDataService();
-  const data = await service.findFirst();
-
-  if (!data) {
-    return (
-      <section className="container mx-auto p-10">
-        <h1 className="text-center text-2xl font-bold">There is no story data to update tags</h1>
-      </section>
-    );
-  }
-
-  return <Form data={data.dataValues} />;
+export default function Home() {
+  return <Form />;
 }
