640e9d41936ddbbda9854dd570ee47e75279cf5d
james-gates-0212 <james.gates.0212@gmail.com>
2024-03-22 06:10:16 -0700

    chore: update load spin🐞
    Co-Authored-By: RR <157709103+OskarMast@users.noreply.github.com>
    Co-Authored-By: mirror0000 <134664641+mirror0000@users.noreply.github.com>

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -7,21 +7,23 @@ import TextArea from '@/components/TextArea';
 import { useEffect, useRef, useState } from 'react';
 
 const LoadSpin = () => (
-  <div className="fixed top-0 right-0 bottom-0 left-0 z-50 bg-black/50 flex flex-row gap-2 items-center justify-center">
-    <svg
-      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-      <path
-        className="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
-    </svg>
-    Processing...
+  <div className="fixed top-0 right-0 bottom-0 left-0 z-50 bg-black/50 flex flex-row items-center justify-center">
+    <div className="flex flex-row gap-2 p-5 rounded-md shadow-md bg-lime-700">
+      <svg
+        className="animate-spin -ml-1 h-5 w-5 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      <span className="text-white font-bold">Processing...</span>
+    </div>
   </div>
 );
 
