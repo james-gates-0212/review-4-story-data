567258e099ffed5db83d5d4a0896ba65f793a06c
james-gates-0212 <james.gates.0212@gmail.com>
2024-03-22 06:10:03 -0700

    fix: establish connection issue🐞
    Co-Authored-By: RR <157709103+OskarMast@users.noreply.github.com>
    Co-Authored-By: mirror0000 <134664641+mirror0000@users.noreply.github.com>

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -2,10 +2,9 @@
  * This module creates the Sequelize to the database and
  * exports all the models.
  */
-import Sequelize from 'sequelize';
+const { Sequelize } = require('sequelize');
 import { getConfig } from '../../config';
 import storyDataModel from './storydata';
-import { createTunnel } from 'tunnel-ssh';
 
 const dataModels = [storyDataModel];
 
@@ -20,55 +19,17 @@ async function models() {
     return null;
   }
 
-  const tunnelOptions = {
-    autoClose: true,
-  };
-
-  const sshOptions = {
-    host: config.SSH_SERVER,
-    port: config.SSH_PORT,
-    username: config.SSH_USER,
-    password: config.SSH_PASSWORD,
-  };
-
-  // Here is where the magic happens...
-  const serverOptions = {
-    host: '127.0.0.1',
-    port: config.DATABASE_PORT,
-  };
-
-  // Note that the forwarding options does not define the srcAddr and srcPort here.
-  // to use the server configuration.
-  const forwardOptions = {
-    srcAddr: config.SSH_SERVER,
-    srcPort: config.DATABASE_PORT,
-    dstAddr: config.DATABASE_HOST,
-    dstPort: config.DATABASE_PORT,
-  };
-
-  const useSSHTunnel = (config.USE_SSH_TUNNEL || '').toLowerCase() === 'true';
-  const useSSL = (config.USE_SSL || '').toLowerCase() === 'true';
-
-  let [server, conn] = useSSHTunnel ? await createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions) : [];
-
-  // Example how to get the server port information.
-  console.log('server listen on', server?.address());
-
   let sequelize = new (<any>Sequelize)(config.DATABASE_DATABASE, config.DATABASE_USERNAME, config.DATABASE_PASSWORD, {
-    host: useSSHTunnel ? '127.0.0.1' : config.DATABASE_HOST,
+    host: config.DATABASE_HOST,
     port: config.DATABASE_PORT,
-    dialect: config.DATABASE_DIALECT,
-    ...(useSSHTunnel || useSSL
-      ? {
-          dialectOptions: {
-            ssl: {
-              require: true, // This will help you. But you will see nwe error
-              rejectUnauthorized: false, // This line will fix new error
-              ca: config.SSL_CERT,
-            },
-          },
-        }
-      : {}),
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        ssl: true,
+        rejectUnauthorized: false,
+        ca: config.SSL_CERT,
+      },
+    },
     logging:
       config.DATABASE_LOGGING === 'true'
         ? (log) =>
